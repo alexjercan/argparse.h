cf48a2518ce41f56b2e6a34adcba4ea9af38b4b3
Alex Jercan <jercan_alex27@yahoo.com>
2024-02-16 07:56:19 +0200

    feat: added positional arguments

--- argparse.h
+++ argparse.h
@@ -1,19 +1,43 @@
 #ifndef ARGPARSE_H
 #define ARGPARSE_H
 
-#define DA_INIT_CAPACITY 8192
-#define DA_REALLOC(oldptr, oldsz, newsz) realloc(oldptr, newsz)
-#define da_append(da, item)                                                    \
+#define ARG_TERMINAL_RED "\033[1;31m"
+#define ARG_TERMINAL_YLW "\033[1;33m"
+#define ARG_TERMINAL_RST "\033[0m"
+
+#define ARG_LOG_WARN(format, ...)                                              \
+    fprintf(stdout,                                                            \
+            ARG_TERMINAL_YLW "WARN" ARG_TERMINAL_RST ": %s:%d: " format "\n",  \
+            __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define ARG_LOG_ERROR(format, ...)                                             \
+    fprintf(stderr,                                                            \
+            ARG_TERMINAL_RED "ERROR" ARG_TERMINAL_RST ": %s:%d: " format "\n", \
+            __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define ARG_PANIC(format, ...)                                                 \
+    do {                                                                       \
+        ARG_LOG_ERROR(format, ##__VA_ARGS__);                                  \
+        exit(1);                                                               \
+    } while (0)
+
+#define ARG_INIT_CAPACITY 8192
+#define ARG_DA_REALLOC(ptr, old_sz, new_sz) realloc(ptr, new_sz)
+#define arg_da_append(da, item)                                                \
     do {                                                                       \
         if ((da)->count >= (da)->capacity) {                                   \
-            size_t new_capacity = (da)->capacity * 2;                          \
+            unsigned int new_capacity = (da)->capacity * 2;                    \
             if (new_capacity == 0) {                                           \
-                new_capacity = DA_INIT_CAPACITY;                               \
+                new_capacity = ARG_INIT_CAPACITY;                              \
             }                                                                  \
                                                                                \
-            (da)->items = DA_REALLOC((da)->items,                              \
-                                     (da)->capacity * sizeof((da)->items[0]),  \
-                                     new_capacity * sizeof((da)->items[0]));   \
+            (da)->items = ARG_DA_REALLOC(                                      \
+                (da)->items, (da)->capacity * sizeof(*(da)->items),            \
+                new_capacity * sizeof(*(da)->items));                          \
+            if ((da)->items == NULL) {                                         \
+                ARG_PANIC("Failed to reallocate dynamic array");               \
+            }                                                                  \
+                                                                               \
             (da)->capacity = new_capacity;                                     \
         }                                                                      \
                                                                                \
@@ -23,6 +47,7 @@
 enum argument_type {
     ARGUMENT_TYPE_VALUE,
     ARGUMENT_TYPE_FLAG,
+    ARGUMENT_TYPE_POSITIONAL,
 };
 
 struct argparse_parser;
@@ -35,6 +60,7 @@ void argparse_add_argument(struct argparse_parser *parser, char short_name,
 void argparse_parse(struct argparse_parser *parser, int argc, char *argv[]);
 char *argparse_get_value(struct argparse_parser *parser, char *long_name);
 unsigned int argparse_get_flag(struct argparse_parser *parser, char *long_name);
+char *argparse_get_positional(struct argparse_parser *parser, char *long_name);
 
 void argparse_print_help(struct argparse_parser *parser);
 void argparse_print_version(struct argparse_parser *parser);
@@ -78,6 +104,11 @@ struct argparse_parser *argparse_new(char *name, char *description,
     parser->count = 0;
     parser->capacity = 0;
 
+    argparse_add_argument(parser, 'v', "version", "print the program version",
+                          ARGUMENT_TYPE_FLAG);
+    argparse_add_argument(parser, 'h', "help", "print help menu",
+                          ARGUMENT_TYPE_FLAG);
+
     return parser;
 }
 
@@ -93,12 +124,22 @@ void argparse_add_argument(struct argparse_parser *parser, char short_name,
         .flag = 0,
     };
 
-    da_append(parser, arg);
+    arg_da_append(parser, arg);
 }
 
 void argparse_parse(struct argparse_parser *parser, int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         char *name = argv[i];
+
+        if (strcmp(name, "-h") == 0 || strcmp(name, "--help") == 0) {
+            argparse_print_help(parser);
+            exit(0);
+        }
+
+        if (strcmp(name, "-v") == 0 || strcmp(name, "--version") == 0) {
+            argparse_print_version(parser);
+            exit(0);
+        }
 
         if (name[0] == '-') {
             struct argument *arg = NULL;
@@ -116,27 +157,62 @@ void argparse_parse(struct argparse_parser *parser, int argc, char *argv[]) {
             }
 
             if (arg == NULL) {
-                fprintf(stderr, "error: invalid argument: %s\n", name);
+                ARG_LOG_ERROR("invalid argument: %s", name);
+                argparse_print_help(parser);
                 exit(1);
             }
 
-            if (arg->type == ARGUMENT_TYPE_FLAG) {
+            switch (arg->type) {
+            case ARGUMENT_TYPE_FLAG: {
                 arg->flag = 1;
-            } else if (arg->type == ARGUMENT_TYPE_VALUE) {
+                break;
+            }
+            case ARGUMENT_TYPE_VALUE: {
                 if (i + 1 >= argc) {
-                    fprintf(stderr, "error: missing value for argument: %s\n",
-                            name);
+                    ARG_LOG_ERROR("missing value for argument: %s", name);
+                    argparse_print_help(parser);
                     exit(1);
                 }
 
                 arg->value = argv[++i];
-            } else {
-                fprintf(stderr, "error: type not supported for argument: %s\n",
-                        name);
+                break;
+            }
+            default: {
+                ARG_LOG_ERROR("type not supported for argument: %s", name);
+                argparse_print_help(parser);
                 exit(1);
             }
+            }
         } else {
-            fprintf(stderr, "error: invalid argument: %s\n", name);
+            struct argument *arg = NULL;
+
+            for (size_t j = 0; j < parser->count; j++) {
+                struct argument *item = &parser->items[j];
+
+                if (item->type == ARGUMENT_TYPE_POSITIONAL &&
+                    item->value == NULL) {
+                    arg = item;
+                    break;
+                }
+            }
+
+            if (arg == NULL) {
+                ARG_LOG_ERROR("unexpected positional argument: %s", name);
+                argparse_print_help(parser);
+                exit(1);
+            }
+
+            arg->value = name;
+        }
+    }
+
+    for (size_t i = 0; i < parser->count; i++) {
+        struct argument *item = &parser->items[i];
+
+        if (item->type == ARGUMENT_TYPE_POSITIONAL && item->value == NULL) {
+            ARG_LOG_ERROR("missing value for positional argument: %s",
+                          item->long_name);
+            argparse_print_help(parser);
             exit(1);
         }
     }
@@ -148,9 +224,9 @@ char *argparse_get_value(struct argparse_parser *parser, char *long_name) {
 
         if (item->long_name != NULL &&
             strcmp(long_name, item->long_name) == 0) {
-            if (item->type != ARGUMENT_TYPE_VALUE) {
-                fprintf(stdout, "warning: argument is not a value: %s\n",
-                        long_name);
+            if (item->type != ARGUMENT_TYPE_VALUE &&
+                item->type != ARGUMENT_TYPE_POSITIONAL) {
+                ARG_LOG_WARN("argument is not a value: %s", long_name);
             }
             return item->value;
         }
@@ -167,8 +243,7 @@ unsigned int argparse_get_flag(struct argparse_parser *parser,
         if (item->long_name != NULL &&
             strcmp(long_name, item->long_name) == 0) {
             if (item->type != ARGUMENT_TYPE_FLAG) {
-                fprintf(stdout, "warning: argument is not a flag: %s\n",
-                        long_name);
+                ARG_LOG_WARN("argument is not a flag: %s", long_name);
             }
             return item->flag;
         }
@@ -178,7 +253,14 @@ unsigned int argparse_get_flag(struct argparse_parser *parser,
 }
 
 void argparse_print_help(struct argparse_parser *parser) {
-    printf("usage: %s [options]\n", parser->name);
+    printf("usage: %s [options]", parser->name);
+    for (size_t i = 0; i < parser->count; i++) {
+        struct argument *item = &parser->items[i];
+
+        if (item->type == ARGUMENT_TYPE_POSITIONAL) {
+            printf(" <%s>", item->long_name);
+        }
+    }
     printf("\n");
     printf("%s\n", parser->description);
     printf("\n");
@@ -187,15 +269,29 @@ void argparse_print_help(struct argparse_parser *parser) {
     for (size_t i = 0; i < parser->count; i++) {
         struct argument *item = &parser->items[i];
 
-        printf("  -%c, --%s", item->short_name, item->long_name);
-
-        if (item->type == ARGUMENT_TYPE_VALUE) {
-            printf(" <value>");
+        switch (item->type) {
+        case ARGUMENT_TYPE_POSITIONAL: {
+            printf("  %c, %s\n", item->short_name, item->long_name);
+            printf("      %s\n", item->description);
+            printf("\n");
+            break;
         }
-
-        printf("\n");
-        printf("      %s\n", item->description);
-        printf("\n");
+        case ARGUMENT_TYPE_FLAG: {
+            printf("  -%c, --%s\n", item->short_name, item->long_name);
+            printf("      %s\n", item->description);
+            printf("\n");
+            break;
+        }
+        case ARGUMENT_TYPE_VALUE: {
+            printf("  -%c, --%s <value>\n", item->short_name, item->long_name);
+            printf("      %s\n", item->description);
+            printf("\n");
+            break;
+        }
+        default: {
+            ARG_PANIC("invalid argument type");
+        }
+        }
     }
 }
 
--- main.c
+++ main.c
@@ -5,36 +5,21 @@
 int main(int argc, char *argv[]) {
     struct argparse_parser *parser =
         argparse_new("test", "test description", "0.0.1");
-    argparse_add_argument(parser, 'v', "version", "print version",
-                          ARGUMENT_TYPE_FLAG);
-    argparse_add_argument(parser, 'h', "help", "print help",
-                          ARGUMENT_TYPE_FLAG);
-    argparse_add_argument(parser, 'f', "file", "file name",
+    argparse_add_argument(parser, 'x', "number", "number",
                           ARGUMENT_TYPE_VALUE);
+    argparse_add_argument(parser, 'f', "file", "file name",
+                          ARGUMENT_TYPE_POSITIONAL);
 
     argparse_parse(parser, argc, argv);
 
-    unsigned int version = argparse_get_flag(parser, "version");
-    unsigned int help = argparse_get_flag(parser, "help");
+    char *number = argparse_get_value(parser, "number");
     char *file = argparse_get_value(parser, "file");
 
-    if (version) {
-        argparse_print_version(parser);
-
-        argparse_free(parser);
-        return 0;
+    if (number) {
+        printf("number: %s\n", number);
     }
 
-    if (help) {
-        argparse_print_help(parser);
-
-        argparse_free(parser);
-        return 0;
-    }
-
-    if (file) {
-        printf("file: %s\n", file);
-    }
+    printf("file: %s\n", file);
 
     argparse_free(parser);
     return 0;
